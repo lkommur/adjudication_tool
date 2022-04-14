bf22796dcf783d31687cd14056f833312c169a3a
lkommur <60636924+lkommur@users.noreply.github.com>
2022-04-14 13:15:01 -0400

    Add files via upload

---
+++ precache-manifest.36f0f03dedc3b40a312f17a686aa5466.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f18e1bba3beb2c309efe1aa96dc3a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576616756f22fd3a7c2b",
+    "url": "/static/css/2.5507d99c.chunk.css"
+  },
+  {
+    "revision": "85a1c0a21edbf3b13265",
+    "url": "/static/css/main.6051288c.chunk.css"
+  },
+  {
+    "revision": "576616756f22fd3a7c2b",
+    "url": "/static/js/2.694da04d.chunk.js"
+  },
+  {
+    "revision": "6fc7f650bef9fab8b353419fee0de048",
+    "url": "/static/js/2.694da04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a1c0a21edbf3b13265",
+    "url": "/static/js/main.e8a7248b.chunk.js"
+  },
+  {
+    "revision": "5477e78cb9059acda7af",
+    "url": "/static/js/runtime-main.ebb1c8c3.js"
+  },
+  {
+    "revision": "0a56871c96445232736b9031973e3051",
+    "url": "/static/media/logo@1x.0a56871c.svg"
+  },
+  {
+    "revision": "763c907e42629356df80b888046bf399",
+    "url": "/static/media/partial-logo-3px.763c907e.svg"
+  },
+  {
+    "revision": "e1a6f82c9a58c8b299ec67046c3e6755",
+    "url": "/static/media/vidAnguishedDeliriousBoar.e1a6f82c.webm"
+  },
+  {
+    "revision": "42f0edb4728b34c8c09ad86246b2b910",
+    "url": "/static/media/vr.42f0edb4.png"
+  }
+]);
